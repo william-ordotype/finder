7522eaa685277651c0e9c15ae5c248cab8f3ccc3
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2023-05-15 19:43:28 +0700

    Update ordotype-index-2023-05-15.js
    Ajout de la colonne Boost

--- ordotype-index-2023-05-15.js
+++ ordotype-index-2023-05-15.js
@@ -148,6 +148,7 @@ async function search(query) {
           query_string: {
             query: query + "*",
             fields: [
+              "Boost^6"
               "Name^5",
               "Alias^4",
               "Ordonnances médicales^3",
