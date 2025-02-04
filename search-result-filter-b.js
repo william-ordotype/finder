882d22eaf09234b249bcdc5df387e63c3892cb95
William <131350789+william-ordotype@users.noreply.github.com>
2025-02-04 09:07:26 +0100

    Update search-result-filter-b.js

--- search-result-filter-b.js
+++ search-result-filter-b.js
@@ -281,7 +281,7 @@ async function displayAll(){
         div.appendChild(img);
     
         resultElement.style.cssText =
-           "text-decoration: none; color: #0c0e16; font-size: 16px; padding: 16px 8px; display: flex; align-items: center; justify-content:space-between";
+           "text-decoration: none; color: #0c0e16; font-size: 16px; padding-top: 16px; padding-bottom: 16px; padding-left: 16px; padding-right: 8px; display: flex; align-items: center; justify-content:space-between";
     
         resultElement.addEventListener("click", function(event) {
             event.preventDefault();
